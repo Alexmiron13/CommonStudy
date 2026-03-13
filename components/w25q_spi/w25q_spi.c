08020965a725c12c35c9f4437cd40f2c6b233823
AlexMIR <namastebro1@gmail.com>
2026-03-13 16:50:04 +0300

    Add W25Q identification

--- components/w25q_spi/w25q_spi.c
+++ components/w25q_spi/w25q_spi.c
@@ -39,6 +39,19 @@
  * Variables
  **************************************************************************************************/
 static const char *TAG = "w25q_spi";
+typedef struct
+{
+  uint16_t  PageSize;
+  uint32_t  PageCount;
+  uint32_t  SectorSize;
+  uint32_t  SectorCount;
+  uint32_t  BlockSize;
+  uint32_t  BlockCount;
+  uint32_t  NumKB;
+  uint8_t   SR1;
+  uint8_t   SR2;
+  uint8_t   SR3;
+}w25_info_t;
 /***************************************************************************************************
  * API
  **************************************************************************************************/
@@ -87,6 +100,7 @@ void W25_Reset (prj_spi_device_t *m_spi_device)
 uint32_t W25_Read_ID(prj_spi_device_t *m_spi_device)
 {
   spi_transaction_t SPITransaction;
+  w25_info_t w25_info;
   uint8_t data[4];
   data[0] = W25_GET_JEDEC_ID;
   memset( &SPITransaction, 0, sizeof( spi_transaction_t ) );
@@ -94,7 +108,70 @@ uint32_t W25_Read_ID(prj_spi_device_t *m_spi_device)
   SPITransaction.tx_buffer = data;
   SPITransaction.rx_buffer = data;
   spi_device_transmit(m_spi_device->spi_dev, &SPITransaction );
-  return (data[1] << 16) | (data[2] << 8) | data[3];
+  uint32_t id = (data[1] << 16) | (data[2] << 8) | data[3];
+  ESP_LOGI(TAG, "ID:0x%X",id);
+ 
+  id &= 0x0000ffff;
+  switch(id)
+  {
+    case 0x401A:
+      w25_info.BlockCount=1024;
+      printf("w25qxx Chip: w25q512\r\n");
+      break;
+    case 0x4019:
+      w25_info.BlockCount=512;
+      printf("w25qxx Chip: w25q256\r\n");
+      break;
+    case 0x4018:
+      w25_info.BlockCount=256;
+      printf("w25qxx Chip: w25q128\r\n");
+      break;
+    case 0x4017:
+      w25_info.BlockCount=128;
+      printf("w25qxx Chip: w25q64\r\n");
+      break;
+    case 0x4016:
+      w25_info.BlockCount=64;
+      printf("w25qxx Chip: w25q32\r\n");
+      break;
+    case 0x4015:
+      w25_info.BlockCount=32;
+      printf("w25qxx Chip: w25q16\r\n");
+      break;
+    case 0x4014:
+      w25_info.BlockCount=16;
+      printf("w25qxx Chip: w25q80\r\n");
+      break;
+    case 0x4013:
+      w25_info.BlockCount=8;
+      printf("w25qxx Chip: w25q40\r\n");
+      break;
+    case 0x4012:
+      w25_info.BlockCount=4;
+      printf("w25qxx Chip: w25q20\r\n");
+      break;
+    case 0x4011:
+      w25_info.BlockCount=2;
+      printf("w25qxx Chip: w25q10\r\n");
+      break;
+    default:
+      printf("w25qxx Unknown ID\r\n");
+      return id;
+  }
+  w25_info.PageSize=256;
+  w25_info.SectorSize=0x1000;
+  w25_info.SectorCount=w25_info.BlockCount*16;
+  w25_info.PageCount=(w25_info.SectorCount*w25_info.SectorSize)/w25_info.PageSize;
+  w25_info.BlockSize=w25_info.SectorSize*16;
+  w25_info.NumKB=(w25_info.SectorCount*w25_info.SectorSize)/1024;
+  printf("Page Size: %d Bytes\r\n",(unsigned int)w25_info.PageSize);
+  printf("Page Count: %u\r\n",(unsigned int)w25_info.PageCount);
+  printf("Sector Size: %u Bytes\r\n",(unsigned int)w25_info.SectorSize);
+  printf("Sector Count: %u\r\n",(unsigned int)w25_info.SectorCount);
+  printf("Block Size: %u Bytes\r\n",(unsigned int)w25_info.BlockSize);
+  printf("Block Count: %u\r\n",(unsigned int)w25_info.BlockCount);
+  printf("Capacity: %u KB\r\n",(unsigned int)w25_info.NumKB);
+  return id;
 }
 
 /***************************************************************************************************
